860967c5da3db2015391303645197ae16f973467
Sam Levin <sam@zinclearninglabs.com>
2022-08-02 15:23:48 +0200

    add random hole generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import './App.css';
 import { Song, Track, Instrument } from 'reactronica'
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
+import _ from "lodash";
+import {exampleSong} from "./songs";
+
+const makeLevel = (notes, numberOfHoles) => {
+  const thisLevel = [...notes]
+  const holes = []
+  while(holes.length < numberOfHoles){
+    const randomHole = _.random(1, notes.length - 1)
+    if(!holes.includes(randomHole) && !holes.includes(randomHole - 1) && !holes.includes(randomHole + 1)){
+      holes.push(randomHole)
+      thisLevel[randomHole] = null
+    }
+  }
+
+  return thisLevel
+}
+
+const colorCycle = [
+  'bg-red-600',
+  'bg-blue-600',
+  'bg-fuchsia-600',
+  'bg-lime-600'
+]
+
+const generateSong = () => {
+  const notes = exampleSong;
+  const firstLevel = makeLevel(notes, 1)
+  const secondLevel = makeLevel(notes, 3)
+  const thirdLevel = makeLevel(notes, 6)
+
+  const fullSong = [...notes, ...firstLevel, ...secondLevel, ...thirdLevel]
+
+  return fullSong.map((n, i) => ({name: n, position: i}))
+}
+
+const songRow = (songSlice, position, isPlaying) => (
+    songSlice.map((note, index) => {
+      if(note.name){
+        return(
+            <div
+                className={`mx-2 w-12 h-8 rounded ease-in ${colorCycle[index % 4]} ${position - 1 === note.position && isPlaying && 'scale-150'}`}/>
+        )
+      } else {
+        return(
+            <div className={`mx-2 w-12 h-8 rounded ease-in `} />
+        )
+      }
+    })
+)
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false)
   const [position, setPosition] = useState(null)
+  const songRef = useRef(generateSong())
 
   const duration = .2
-
-  const notes = [
-    'C2', 'E2', 'G2', 'C3',
-    'D2', 'F2', 'A2', 'D3',
-    'F2', 'G2', 'B2', 'F3',
-    'G2', 'B2', 'C3', 'E3'
-  ]
-
-  const colorCycle = [
-      'bg-red-600',
-      'bg-blue-600',
-      'bg-fuchsia-600',
-      'bg-lime-600'
-  ]
 
   const startPlaying = () => {
     setPosition(0)
@@ -34,18 +70,25 @@ function App() {
         <div className="my-10">
           <button className="bg-fuchsia-700 text-white text-2xl p-4 translate-y-0" onClick={startPlaying}>Play</button>
         </div>
-        <div className="flex">
-          { notes.map((note, index) => (
-              <div className={`mx-2 w-12 h-8 rounded ease-in ${ colorCycle[index % 4]} ${ position - 1 === index && isPlaying && 'scale-150' }`} />
-          ))}
+        <div className="flex my-2">
+          { songRow(songRef.current.slice(0, 16), position, isPlaying)}
+        </div>
+        <div className="flex my-2">
+          { songRow(songRef.current.slice(16, 32), position, isPlaying)}
+        </div>
+        <div className="flex my-2">
+          { songRow(songRef.current.slice(32, 48), position, isPlaying)}
+        </div>
+        <div className="flex my-2">
+          { songRow(songRef.current.slice(48, 64), position, isPlaying)}
         </div>
         <Song isPlaying={isPlaying} bpm={240} volume={0} isMuted={false}>
           <Track
-              steps={notes.map((n, i, a) => ({ name: n, duration: duration, velocity: 1}))}
+              steps={songRef.current.map((n, i, a) => ({ ...n, duration: duration, velocity: 1}))}
               onStepPlay={(step) => {
                 setPosition((lastPosition) => {
                   const newPosition = lastPosition + 1
-                  if(newPosition >= notes.length){
+                  if(newPosition >= songRef.current.length){
                     setTimeout(() => setIsPlaying(false), duration * 1000)
                     return newPosition
                   } else {
---
+++ src/songs.js
@@ -0,0 +1,6 @@
+export const exampleSong = [
+    'C2', 'E2', 'G2', 'C3',
+    'D2', 'F2', 'A2', 'D3',
+    'F2', 'G2', 'B2', 'F3',
+    'G2', 'B2', 'C3', 'E3'
+]
