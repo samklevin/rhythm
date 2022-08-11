74cf390e1d5ce1a2ecfe73b825c2a5f114771299
Sam Levin <sam@zinclearninglabs.com>
2022-08-11 12:52:38 +0200

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import {Song, Track, Instrument, Effect} from 'reactronica'
 import React, {useEffect, useRef, useState} from "react";
 import _ from "lodash";
 import {exampleSong} from "./songs";
-import {SongPlayer} from "./SongPlayer";
 import * as Tone from 'tone'
 
 const makeLevel = (notes, numberOfHoles) => {
-  const thisLevel = [...notes]
+  const thisLevel = notes.slice()
   const holes = []
   let iterations = 0
   while(holes.length < numberOfHoles){
@@ -20,7 +18,7 @@ const makeLevel = (notes, numberOfHoles) => {
         if(holes.includes(randomHole + 2)){ continue }
       }
       holes.push(randomHole)
-      thisLevel[randomHole] = null
+      thisLevel[randomHole].play = false
     }
     iterations++
     if(iterations > notes.length * 2){ break }
@@ -28,6 +26,12 @@ const makeLevel = (notes, numberOfHoles) => {
 
   return thisLevel
 }
+
+const shiftUpTwoOctaves = (note) => (
+    note.replace(/\d/, (octave) => (
+        Number(octave) + 2
+    ))
+)
 
 const colorCycle = [
   'bg-red-600',
@@ -39,8 +43,8 @@ const colorCycle = [
 const invertLevel = (notes, startingLevel) => {
   const invertedLevel = [...notes]
   startingLevel.forEach((e, i, a) => {
-    if(!e){
-      invertedLevel[a.length - (1 + i)] = null
+    if(e.play === false){
+      invertedLevel[a.length - (1 + i)].play = false
     }
   })
   return invertedLevel
@@ -48,26 +52,30 @@ const invertLevel = (notes, startingLevel) => {
 
 const addTimeValues = (song) => (
     song.map((note, i) => ({
-      note,
+      ...note,
       time: `${ Math.floor(i / 16)}:${ Math.floor((i % 16) / 4)}:${ i % 4 }`
     })
     )
 )
 
-const generateSong = () => {
-  const notes = exampleSong;
-  const shuffledNotes = _.shuffle([...notes])
+const serializedNotes = () => (
+    exampleSong.map(n => ({ note: n, play: true}))
+)
 
-  const firstLevel = makeLevel(_.reverse([...notes]), 3)
-  const secondLevel = makeLevel(notes, 5)
-  const thirdLevel = makeLevel(shuffledNotes, 6)
-  const fourthLevel = makeLevel(notes, 6)
+
+const generateSong = () => {
+  const notes = serializedNotes()
+
+  const firstLevel = makeLevel(_.reverse(serializedNotes()), 3)
+  const secondLevel = makeLevel(serializedNotes(), 5)
+  const thirdLevel = makeLevel(_.shuffle(serializedNotes()), 6)
+  const fourthLevel = makeLevel(serializedNotes(), 6)
   const fifthLevel = invertLevel(_.reverse([...notes]), fourthLevel)
-  const sixthLevel = makeLevel(notes, 9)
-  const seventhLevel = invertLevel(shuffledNotes, sixthLevel)
+  const sixthLevel = makeLevel(serializedNotes(), 9)
+  const seventhLevel = invertLevel(_.shuffle(serializedNotes()), sixthLevel)
 
   let fullSong = [
-      ...notes,
+      ...serializedNotes(),
       ...firstLevel,
       ...secondLevel,
       ...thirdLevel,
@@ -75,7 +83,7 @@ const generateSong = () => {
       ...fifthLevel,
       ...sixthLevel,
       ...seventhLevel,
-      ..._.reverse([...notes])
+      ..._.reverse(serializedNotes())
   ]
 
   fullSong = addTimeValues(fullSong).map((n, i) => ({...n, position: i}))
@@ -83,13 +91,18 @@ const generateSong = () => {
 
 }
 
-const songRow = (songSlice, position, isPlaying) => (
+const songRow = (songSlice, position, isPlaying, hits) => (
     songSlice.map((note, index) => {
-      if(note.note){
+      if(note.play){
         return(
             <div
                 key={`note-${ note.position }`}
                 className={`mx-2 w-12 h-8 rounded ease-in ${colorCycle[index % 4]} ${note.time === Tone.Transport.position.split('.')[0] && isPlaying && 'scale-150'}`}/>
+        )
+      } else if(hits.includes(note.time)) {
+        return(
+            <div key={`note-${ note.position }`}
+                 className='mx-2 w-12 h-8 rounded ease-in bg-white animate-pulse' />
         )
       } else {
         return(
@@ -103,54 +116,67 @@ const songRow = (songSlice, position, isPlaying) => (
 function App() {
   const [isPlaying, setIsPlaying] = useState(false)
   const [position, setPosition] = useState(null)
-  const [userNotes, setUserNotes] = useState([])
+  const [hits, setHits] = useState([])
   const songRef = useRef(generateSong())
-
-  const duration = .12
 
   const startPlaying = async () => {
     setPosition(0)
     setIsPlaying(true)
     await Tone.start()
-    Tone.Transport.bpm.value = 60;
+    Tone.Transport.bpm.value = 68;
     Tone.Transport.start()
   }
 
-  const updatePosition = () => {
-    setPosition(() => {
-      const newPosition = Tone.Transport.position.split('.')[0]
-      if(newPosition === songRef.current.slice(-1)[0].time){
-        setTimeout(() => setIsPlaying(false), duration * 1000)
-        return newPosition
-      } else {
-        return newPosition
+  const updatePosition = (time) => {
+    setPosition(time)
+  }
+
+  useEffect(() => {
+    const songSynth = new Tone.Synth().toDestination();
+    songSynth.volume.value = -12
+    songRef.current.forEach((n, i, a) => {
+      Tone.Transport.schedule(() => {
+        updatePosition(n.time)
+      }, n.time)
+      if(i === a.length - 1){
+        setIsPlaying(false)
       }
     })
-  }
+    new Tone.Part((
+        (time, value) => {
+          if(value.play){
+            songSynth.triggerAttackRelease(value.note, '16n', time)
+          }
+        }
+    ), songRef.current).start(0)
+  }, [songRef.current.length])
 
   useEffect(() => {
 
     document.addEventListener('keydown', userKeyDown);
-    document.addEventListener('keyup', userKeyUp);
 
     return () => {
       document.removeEventListener('keydown', userKeyDown);
-      document.removeEventListener('keyup', userKeyUp);
     }
   })
 
+  const playerSynth = new Tone.Synth().toDestination()
+
   const userKeyDown = (event) => {
     if(event.keyCode === 74 && isPlaying){
-      const currentNote = exampleSong[position % 16]
-      setUserNotes([{name: currentNote, duration, velocity: 1}])
+      const currentPosition = Tone.Transport.position.split('.')
+      const position = currentPosition[0]
+      const remainder = currentPosition[1]
+      const currentNote = songRef.current.find(n => n.time === position)
+      if(!currentNote.play && remainder < 800){
+        playerSynth.triggerAttackRelease(shiftUpTwoOctaves(currentNote.note), '16n', Tone.context.currentTime)
+        setHits((prevHits) => [...prevHits, position])
+      }
     }
     if(event.keyCode === 75){
-      setUserNotes([{name: 'D4'}])
+      playerSynth.triggerAttackRelease("D5", '8n', Tone.context.currentTime);
     }
-  }
 
-  const userKeyUp = () => {
-    setUserNotes([])
   }
 
   return (
@@ -163,16 +189,10 @@ function App() {
         {
           [...Array(Math.floor(songRef.current.length / 16))].map((_, i) => (
               <div key={`row-${ i }`} className="flex my-2">
-                { songRow(songRef.current.slice(i * 16, (i+1)*16), position, isPlaying)}
+                { songRow(songRef.current.slice(i * 16, (i+1)*16), position, isPlaying, hits)}
               </div>
           ))
         }
-        <SongPlayer song={songRef.current} updatePosition={updatePosition} />
-        <Song>
-          <Track>
-            <Instrument type="synth" notes={userNotes} />
-          </Track>
-        </Song>
       </header>
     </div>
   );
--- src/SongPlayer.jsx
+++ src/SongPlayer.jsx
@@ -1,18 +1,22 @@
-import {Effect, Instrument, Song, Track} from "reactronica";
 import React from "react";
-import {exampleBassLine} from "./songs";
 import * as Tone from 'tone'
 
-export const SongPlayer = ({ song, updatePosition }) => {
+export const SongPlayer = ({ song, updatePosition, setIsPlaying }) => {
     const synth = new Tone.Synth().toDestination();
-    song.forEach((n, i) => {
+    synth.volume.value = -12
+    song.forEach((n, i, a) => {
         Tone.Transport.schedule(() => {
-            updatePosition()
+            updatePosition(n.time)
         }, n.time)
+        if(i === a.length - 1){
+            setIsPlaying(false)
+        }
     })
     new Tone.Part((
         (time, value) => {
-            synth.triggerAttackRelease(value.note, '8n', time)
+            if(value.play){
+                synth.triggerAttackRelease(value.note, '16n', time)
+            }
         }
     ), song).start(0)
 }
