708e4beca4ada5f34088a5b12251fa2a08071065
Sam Levin <sam@zinclearninglabs.com>
2022-09-06 10:23:56 +0200

    refactor playerSynth

--- src/components/PlayerSynth.js
+++ src/components/PlayerSynth.js
@@ -13,30 +13,32 @@ const PlayerSynth = ({ lockedAt, setLockedAt, setHits, songRef }) => {
 
   const playerSynth = new Tone.PolySynth(Tone.Synth).toDestination();
 
+  const playWrongNote = (now) => {
+    playerSynth.triggerAttack("A3", now);
+    playerSynth.triggerAttack("Bb3", now);
+    playerSynth.triggerAttack("B3", now);
+    playerSynth.triggerRelease(["A3", "Bb3", "B3"], now + 0.1);
+  };
+
+  const playHit = (now, note) => {
+    playerSynth.triggerAttackRelease(shiftUpTwoOctaves(note), "16n", now);
+  };
+
   const userKeyDown = (event) => {
     if (lockedAt) {
       return;
     }
     if (event.keyCode === 32) {
-      const currentPosition = Tone.Transport.position.split(".");
-      const position = currentPosition[0];
-      const remainder = currentPosition[1];
-      const currentNote = songRef.current.find((n) => n.time === position);
+      const [transportTime, remainder] = Tone.Transport.position.split(".");
+      const currentNote = songRef.current.find((n) => n.time === transportTime);
       const now = Tone.context.currentTime;
       if (!currentNote.play && remainder < 800) {
-        playerSynth.triggerAttackRelease(
-          shiftUpTwoOctaves(currentNote.note),
-          "16n",
-          now
-        );
-        setHits((prevHits) => [...prevHits, position]);
+        playHit(now, currentNote.note);
+        setHits((prevHits) => [...prevHits, transportTime]);
         setLockedAt(null);
       } else {
         setLockedAt(now);
-        playerSynth.triggerAttack("A3", now);
-        playerSynth.triggerAttack("Bb3", now);
-        playerSynth.triggerAttack("B3", now);
-        playerSynth.triggerRelease(["A3", "Bb3", "B3"], now + 0.1);
+        playWrongNote(now);
       }
     }
   };
